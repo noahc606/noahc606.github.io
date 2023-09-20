5f2ba034a7d800aae4e61ac7fbdaa1cd1c4db4d4
noahc606 <noah.c.h@outlook.com>
2023-09-20 19:55:13 -0400

    Header/footer scripting and tweaks
    Added JS classes for header and footer to avoid having to copy header/footer every time.
    Added <hr> after every <h2>

---
+++ itis3135/components/nch-footer.js
@@ -0,0 +1,33 @@
+class NCH_Footer extends HTMLElement {
+    constructor() {
+        super();
+    }  
+
+    connectedCallback() {
+        this.innerHTML = `
+        <div class="nch-div-background">
+            <div class="nch-div-footer nch-div-floating">
+                <nav>
+                    <a href="https://webpages.charlotte.edu/nhebert1/">UNC Charlotte Webspace</a> |
+                    <a href="https://github.com/noahc606/noahc606.github.io">Course Github Repo</a> |
+                    <a href="https://noahc606.github.io/">Course Github.io</a> |
+                    <a href="https://www.freecodecamp.org/noahc606">freeCodeCamp</a> |
+                    <a href="https://www.codecademy.com/profiles/noahc606">Codecademy</a> |
+                    <a href="https://www.linkedin.com/in/nh606/">LinkedIn Profile</a>
+                </nav>
+                <p>© 2023 Designed by <a href="hi.biz/">Hebert, Inc.</a></p>
+                <a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Fwebpages.charlotte.edu%2Fnhebert1%2Fitis3135%2F">
+                    <img style="border:0;width:88px;height:31px" src="https://upload.wikimedia.org/wikipedia/commons/b/bb/W3C_HTML5_certified.png" alt="Valid HTML!">
+                </a>
+                <a href="https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fwebpages.charlotte.edu%2Fnhebert1%2Fitis3135%2F">
+                    <img style="border:0;width:88px;height:31px" src="https://jigsaw.w3.org/css-validator/images/vcss" alt="ValidCSS!">
+                </a>
+            </div>
+        </div>
+        <div class="nch-div-background-alt">
+        </div>
+        `;
+    }
+}
+
+customElements.define('nch-footer', NCH_Footer);
---
+++ itis3135/components/nch-header.js
@@ -0,0 +1,31 @@
+class NCH_Header extends HTMLElement {
+    constructor() {
+        super();
+    }  
+
+    connectedCallback() {
+        this.innerHTML = `
+        <div class="nch-div-background">
+            <div class="nch-div-header nch-div-floating nch-text-center">
+                <h1>Noah Hebert's ITIS3135 Site</h1>
+            </div>
+            <div class="nch-div-header nch-div-floating">
+                <nav>
+                    <a class="nch-bold" href="index.html">Home</a> |
+                    <a href="introduction.html">Introduction</a> |
+                    <a href="contract.html">Contract</a> |
+                    <a href="tables.html">Tables</a> |
+                    <a href="forms.html">Forms</a> |
+                    <a href="website_evaluations.html">Website Evaluations</a> |
+                    <a href="../">Back to Parent Site</a>
+                </nav>
+                <nav>
+                    <a href="stuff/( ͡❛ ͜ʖ ͡❛) A WEBPAGE file!.htm">Bad Design</a>
+                </nav>
+            </div>
+        </div>
+        `;
+    }
+}
+
+customElements.define('nch-header', NCH_Header);
