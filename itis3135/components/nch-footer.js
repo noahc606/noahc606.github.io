420966fa7e74739077983b11c322b28f491f79c7
noahc606 <noah.c.h@outlook.com>
2023-10-03 23:15:50 -0400

    Validation buttons now open in new tab
    Also fixed validation for one page

--- itis3135/components/nch-footer.js
+++ itis3135/components/nch-footer.js
@@ -16,10 +16,10 @@ class NCH_Footer extends HTMLElement {
                     <a href="https://www.linkedin.com/in/nh606/">LinkedIn Profile</a>
                 </nav>
                 <p>© 2023 Designed by <a href="hi.biz/">Hebert, Inc.</a></p>
-                <a href="https://validator.w3.org/nu/?doc=`+window.location.href+`">
+                <a href="https://validator.w3.org/nu/?doc=`+window.location.href+`" target="_blank">
                     <img style="border:0;width:88px;height:31px" src="https://upload.wikimedia.org/wikipedia/commons/b/bb/W3C_HTML5_certified.png" alt="Valid HTML!">
                 </a>
-                <a href="https://jigsaw.w3.org/css-validator/validator?uri=`+window.location.href+`">
+                <a href="https://jigsaw.w3.org/css-validator/validator?uri=`+window.location.href+`" target="_blank">
                     <img style="border:0;width:88px;height:31px" src="https://jigsaw.w3.org/css-validator/images/vcss" alt="ValidCSS!">
                 </a>
             </div>
