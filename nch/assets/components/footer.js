eca584d99a7eec1b20bc66472a7238ccffae1c35
Noah Hebert <noah.c.h@outlook.com>
2024-10-02 12:34:10 -0400

    fix

--- nch/assets/components/footer.js
+++ nch/assets/components/footer.js
@@ -1,5 +1,5 @@
-let siteVersion = "1.1.3";
-let lastUpdate = "12:33pm, 2 October 2024";
+let siteVersion = "1.1.4";
+let lastUpdate = "12:35pm, 2 October 2024";
 
 class NCH_Footer extends HTMLElement {
     constructor() {
